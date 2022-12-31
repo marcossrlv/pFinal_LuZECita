5683e2ba4c0d0cfe9628cf742db54fc546400bc2
Marcos <72473635+marcossrlv@users.noreply.github.com>
2022-12-31 14:44:34 +0100

    Parte de AccionesTecnico

--- practicaFinal.c
+++ practicaFinal.c
@@ -6,14 +6,14 @@
 #include <time.h>
 
 pthread_t tec1, tec2, resp1, resp2, enc, aten;
-pthread_mutex_t solicitudes, fichero, colaClientes, solicitudes,solsDomicilarias;
+pthread_mutex_t solicitudes, fichero, colaClientes,solsDomicilarias;
 pthread_cond_t suficientesSolicitudes;
 int nSolsDomiciliarias, nClientesApp, nClientesRed;
 struct cliente clientes[20];
 FILE logfile;
 
 struct cliente {
-    int id, prioridad, atendido, solicitud;
+    int id, prioridad, atendido, solicitud, tipo;
 };
 
 
@@ -117,6 +117,78 @@ void AccionesCliente(){
 
 void AccionesTécnico(){
 
+    struct cliente* clienteElegido;
+    int n;
+    int encontrado;
+
+    while(true) {
+
+        n=0;
+        encontrado=0;
+
+        pthread_mutex_lock(&colaClientes);
+
+        //se busca un cliente de tipo app sin atender por el que empezar a comparar
+        for(int i=0; i<20 && encontrado=0; i++){
+            if(clientes[i].tipo==app && clientes[i].atendido==0){
+                clienteElegido = &clientes[i];
+                encontrado = 1;
+                n = i;
+            }
+        }
+
+        //si hay algun cliente de tipo app y no atendido se empieza la comparación con el resto
+        if(encontrado==1){
+            //se empieza a comparar desde la posicion del primero que se encontró
+            for(int i=n+1; i<20; i++) {
+                if(clientes[i].tipo == app && clientes[i].atendido==0) {
+                    //se comparan prioridades, si son iguales se coge al primero de la lista que llevará más tiempo esperando??
+                    if(clientes[i].prioridad > *clienteElegido->prioridad) {
+                        clienteElegido = &clientes[i];
+                    }
+                }
+            }
+
+            *clienteElegido->atendido = 1;
+            pthread_mutex_unlock(&colaClientes);
+
+            int tipoAtencion = calculaAleatorios(0,100);
+            int tiempoAtencion = 0;
+
+            if(tipoAtencion <= 80) {
+                //80% bien identificados
+                tiempoAtencion = calculaAleatorios(1,4);
+            } else if (tipoAtencion > 80 && tipoAtencion <= 90) {
+                //10% mal identificados
+                tiempoAtencion = calculaAleatorios(2,6);
+            } else {
+                //10% confusion de compañia, abandonan sistema
+                tiempoAtencion = calculaAleatorios(1,2)
+            }
+
+            pthread_mutex_lock(&fichero);
+            writeLogMessage("Comienza la atención:\n");
+            pthread_mutex_unlock(&fichero);
+
+            sleep(tiempoAtencion);
+
+            pthread_mutex_lock(&fichero);
+            writeLogMessage("Finaliza la atención:\n");
+            writeLogMessage("motivo final de atencion");
+            pthread_mutex_unlock(&fichero);
+
+            pthread_mutex_lock(&colaClientes);
+            *clienteElegido->atendido
+            pthread_mutex_unlock(&colaClientes);
+
+
+        } else {
+            pthread_mutex_unlock(&colaClientes);
+            sleep(1);
+        }
+        
+    }
+
 }
 
 void AccionesEncargado(){
@@ -125,6 +197,10 @@ void AccionesEncargado(){
 
 void AccionesTecnicoDomiciliario(){
 
+}
+
+int calculaAleatorios(int min, int max) {
+	return rand() % (max-min+1) + min;
 }
 
 
